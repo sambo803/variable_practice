71ea570097e9ea264c4033c59fcf259081b036e3
Samuel Bacon <sbacon803@gmail.com>
2022-04-19 23:22:08 -0600

    Add variables Integers+Floats

--- variables.rb
+++ variables.rb
@@ -3,4 +3,12 @@ user_name = "John"
 email = "hagatha@gmail.com"
 favorite_food = "Pizza"
 
-# Variables in Ruby: 
+# Variables in Ruby: Integers
+starting_age = 31
+jumping_off_age = 53
+years_of_service = 22
+
+# Variables in Ruby: Floats
+gpa = 3.33
+batting_avg = .407
+years_of_service = 22.003
